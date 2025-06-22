3feeeb503ccf5c01ce0c9227b03eb1f03bbb6fe0
Rafael Leite <rafaelbarrosleite@gmail.com>
2025-06-22 17:28:29 -0300

    Add models and new route Create task

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 app = Flask(__name__)
 
-@app.route("/")
-def hello_word():
-    return "Hello Word!"
+#CRUD - Create, Read, Update e Delete
 
-@app.route("/about")
-def aboout():
-    return "Página sobre"
+tasks = []
+task_id_control = 1
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id= task_id_control, title=data['title'], description=data.get("description", ""))
+    print(data)
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    ##return "Nova tarefa criada com sucesso"
+    return jsonify({"messagem": "Nova tarefa criada com sucesso"})
 
 #utilizado debug somente para rodar local o __main__ significa que está local sendo executado manualmente
 if __name__== "__main__":
